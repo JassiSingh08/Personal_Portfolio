fe6cc994dc3c0c17ebc7aef26e67923b5fb8705f
JassiSingh08 <singhjassi693@gmail.com>
2023-09-03 18:32:53 +0530

    switch for Dark theme

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Socials from './components/Socials';
 import Home from "./components/Home";
 import Navbar from './components/Navbar';
 import Skills from './components/Skills';
+// import LightToDark from './components/LightToDark';
 
 function App() {
   return (
     <>
     <Navbar/>
     <Socials />
+    {/* <LightToDark/> */}
     <Home />
     <Skills/>
     </>
---
+++ src/components/LightToDark.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const LightToDark = () => {
+  return (
+    <div>
+      <label for="theme" className="theme">
+        <span className="theme__toggle-wrap">
+          <input
+            id="theme"
+            className="theme__toggle"
+            type="checkbox"
+            role="switch"
+            name="theme"
+            value="dark"
+          />
+          <span className="theme__fill"></span>
+          <span className="theme__icon">
+            <span className="theme__icon-part"></span>
+            <span className="theme__icon-part"></span>
+            <span className="theme__icon-part"></span>
+            <span className="theme__icon-part"></span>
+            <span className="theme__icon-part"></span>
+            <span className="theme__icon-part"></span>
+            <span className="theme__icon-part"></span>
+            <span className="theme__icon-part"></span>
+            <span className="theme__icon-part"></span>
+          </span>
+        </span>
+      </label>
+    </div>
+  );
+}
+
+export default LightToDark
