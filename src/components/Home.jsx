737bb72c8b37315ec5b55486a06bb7127b068cb9
JassiSingh08 <singhjassi693@gmail.com>
2023-09-04 16:49:22 +0530

    updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,49 @@
-import React from 'react'
-import profile_pic from "../Assets/Profile_Pic.png"
-import MERN from './MERN';
+import React from "react";
+import profile_pic from "../Assets/Profile_Pic.png";
+import MERN from "./MERN";
+import { TypeAnimation } from "react-type-animation";
 
 const Home = () => {
   return (
     <>
-    <div className="home-container">
-      <div className="info-container">
-        <div className="first-info">
-          <h1>JASSI SINGH</h1>
+      <div className="home-container">
+        <div className="info-container">
+          <div className="first-info">
+            <h1>JASSI SINGH</h1>
+          </div>
+          <div className="second-info">
+            <p>
+              <TypeAnimation
+                sequence={[
+                  // Same substring at the start will only be typed out once, initially
+                  "Frontend Developer",
+                  1000, // wait 1s before replacing
+                  "Backend Developer",
+                  1000,
+                  "Sometimes Both ~",
+                  500,
+                  "FullStack Developer",
+                  1000,
+                ]}
+                wrapper="span"
+                speed={50}
+                style={{ fontSize: "2em", display: "inline-block" }}
+                repeat={Infinity}
+              />
+            </p>
+          </div>
+          <div className="third-info">
+            I build accessible, inclusive products and digital experiences for
+            the web.
+          </div>
         </div>
-        <div className="second-info">
-          <h2>FULL STACK DEVELOPER</h2>
-        </div>
-        <div className="third-info">
-          I build accessible, inclusive products and digital experiences for the
-          web.
+        <div className="profile-pic-container">
+          <img src={profile_pic} alt="profile_pic" className="profile-pic" />
         </div>
       </div>
-      <div className="profile-pic-container">
-        <img src={profile_pic} alt="profile_pic" className="profile-pic" />
-      </div>
-    </div>
-      <MERN/>
-      </>
+      <MERN />
+    </>
   );
-}
+};
 
-export default Home
+export default Home;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 import { TypeAnimation } from "react-type-animation";
+import html from "../Assets/HTML.png";
+import css from "../Assets/css.png";
+import js from "../Assets/js.png";
+import Mongodb from "../Assets/mongodb.png";
+import express from "../Assets/Express.png";
+import reactPNG from "../Assets/React.png";
+import node from "../Assets/node-js.svg";
+import bootstrap from "../Assets/Bootstrap.png";
+import git from "../Assets/GIT.png";
+import postman from "../Assets/Postman.png";
+import Restapi from "../Assets/RESTAPI.jpg";
+import mssql from "../Assets/SQL SERVER.png";
 
 const Skills = () => {
   return (
@@ -15,7 +27,7 @@ const Skills = () => {
                 1000, // wait 1s before replacing
                 "OH!",
                 700,
-                "Languages I Speak" ,
+                "Languages I Speak",
                 2000,
               ]}
               wrapper="span"
@@ -27,7 +39,20 @@ const Skills = () => {
           </p>
         </div>
         <div class="content">
-          <p class="heading"></p>
+          <div className="skill-pic-container">
+            <img className="skill-pic" src={html} alt="html" />
+            <img className="skill-pic" src={css} alt="html" />
+            <img className="skill-pic" src={bootstrap} alt="html" />
+            <img className="skill-pic" src={js} alt="html" />
+            <img className="skill-pic" src={reactPNG} alt="html" />
+            <img className="skill-pic" src={node} alt="html" />
+            <img className="skill-pic" src={express} alt="html" />
+            <img className="skill-pic" src={Mongodb} alt="html" />
+            <img className="skill-pic" src={mssql} alt="html" />
+            <img className="skill-pic" src={git} alt="html" />
+            <img className="skill-pic" src={Restapi} alt="html" />
+            <img className="skill-pic" src={postman} alt="html" /> 
+          </div>
           <p></p>
         </div>
       </div>
