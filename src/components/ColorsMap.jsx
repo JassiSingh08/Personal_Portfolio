60a501e022e0da03496baa03768de0d6446b0c0e
JassiSingh08 <singhjassi693@gmail.com>
2023-09-12 16:44:28 +0530

    Updates

---
+++ src/components/ColorsMap.jsx
@@ -0,0 +1,74 @@
+import React, { useContext } from "react";
+import { TwitterPicker } from "react-color";
+import { DarkContext } from "../context/ThemeContext";
+
+const ColorPicker = () => {
+  const { toggleAccentColors } = useContext(DarkContext);
+
+  const handleColorChange = (newColor) => {
+    toggleAccentColors(newColor.hex);
+  };
+
+  const CustomPresets = [
+    "#ef9a9a",
+    "#e57373",
+    "#ef5350",
+    "#f44336",
+    "#f48fb1",
+    "#f06292",
+    "#ec407a",
+    "#e91e63",
+    "#ce93d8",
+    "#ba68c8",
+    "#ab47bc",
+    "#9c27b0",
+    "#b39ddb",
+    "#9575cd",
+    "#7e57c2",
+    "#673AB7",
+    "#9FA8DA",
+    "#7986CB",
+    "#5C6BC0",
+    "#3F51B5",
+    "#90CAF9",
+    "#64B5F6",
+    "#42A5F5",
+    "#2196F3",
+    "#81D4FA",
+    "#4FC3F7",
+    "#29B6F6",
+    "#03A9F4",
+    "#80DEEA",
+    "#4DD0E1",
+    "#26C6DA",
+    "#00BCD4",
+    "#80CBC4",
+    "#4DB6AC",
+    "#26A69A",
+    "#009688",
+    "#A5D6A7",
+    "#81C784",
+    "#66BB6A",
+    "#4CAF50",
+    "#C5E1A5",
+    "#AED581",
+    "#9CCC65",
+    "#8BC34A",
+    "#E6EE9C",
+    "#DCE775",
+    "#D4E157",
+    "#CDDC39",
+  ];
+
+  return (
+    <div>
+      <TwitterPicker
+        onChange={handleColorChange}
+        colors={CustomPresets}
+        triangle="hide"
+      />
+    </div>
+  );
+};
+
+export default ColorPicker;
--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,18 +1,22 @@
-import React, { useRef } from "react";
+import React, { useContext, useRef, useState } from "react";
 import Contact from "../Assets/AboutmePIC.png";
 import emailjs from '@emailjs/browser';
+import { DarkContext } from "../context/ThemeContext";
 
 const ContactMe = () => {
+
+  const {darkMode} = useContext(DarkContext)
+  const [isMessageSent, setMessageSent] = useState(false);
 
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
-
     emailjs.sendForm('service_sqqqwbo', 'template_h3ihazn', form.current, 'MqeF1i118qwy3gyu1')
       .then((result) => {
           console.log(result.text);
           e.target.reset();
+          setMessageSent(true)
       }, (error) => {
           console.log(error.text);
       });
@@ -20,8 +24,8 @@ const ContactMe = () => {
 
 
   return (
-    <div className="contact-me">
-      <div className="contact-container">
+    <div className={`contact-me`}>
+      <div className={`contact-container  ${darkMode ? "darkTheme" : ""}`}>
         <form className="form" ref={form} onSubmit={sendEmail}>
           <div className="descr cm-title">Hey, Let's Connect!</div>
           <div className="contact-input">
@@ -52,7 +56,7 @@ const ContactMe = () => {
             />
             <label htmlFor="message">Message</label>
           </div>
-          <button type="submit" value="Send">Send Message →</button>
+          <button style={{ color: darkMode ? '#fff' : '#000' }} type="submit"> {isMessageSent ? "Message Sent ✔" : "Send Message →"}</button>
         </form>
         <div className="cm-img">
           <img src={Contact} alt="Contact" />
